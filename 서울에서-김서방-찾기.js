603d5f37f013aad0d8906859ea06020cbe07cfa2
LeePhoter <estarg51@gmail.com>
2021-12-06 13:59:14 +0900

    1주차 우애-과재

--- 서울에서-김서방-찾기.js
+++ 서울에서-김서방-찾기.js
@@ -3,8 +3,5 @@ function solution(text1) {
     text1.forEach((item, index) => {
         item === "Kim" ? (answer = `김서방은 ${index}에 있다`) : answer;
     });
-    console.log(answer);
     return answer;
 }
-let seoul = ["Jane", "Kim"];
-solution(seoul);
