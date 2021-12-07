b7d3facb6940e3aa59a5e11ca9e4999648a78104
leephoter <estarg51@gmail.com>
2021-12-07 11:50:47 +0900

    2일차 우애 - 과제

---
+++ 문자열-내-p와-y의-개수.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = false;
+    let counts = {
+        p: 0,
+        y: 0,
+    };
+    for (var i = 0; i <= s.length; i++) {
+        s[i] === "P" || s[i] === "p" ? (counts["p"] += 1) : counts;
+        s[i] === "Y" || s[i] === "y" ? (counts["y"] += 1) : counts;
+    }
+    counts["p"] === counts["y"] ? (answer = true) : (answer = false);
+    return answer;
+}
