34f793e953568cc0952d4901f2deaf1e1ec0a347
송현경 <shg@songhyeongyeong-ui-MacBookPro.local>
2021-12-07 11:59:20 +0900

    songhyungyoung: 서울에서 김서방 찾기 풀이

--- 서울에서-김서방-찾기.js
+++ 서울에서-김서방-찾기.js
@@ -3,4 +3,4 @@ function solution(seoul) {
     return '김서방은 ' + seoul.indexOf('Kim') + '에 있다';
 }
       
-console.log(solution(["song", "hyun","gyoung", "Kim"]));
+//console.log(solution(["song", "hyun","gyoung", "Kim"]));
